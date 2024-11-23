bcec38511a6238904e2bf271c3df60650fc3dbd5
oatmealsoup <alexander.aultman@gmail.com>
2024-11-23 17:47:04 -0500

    Added getElementById(lastKey); and put the objects in a form so that pressing the mouseEventButton will trigger an alert containing the typed text

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const mouseEventButton = document.getElementById('mouseEventButton')
-
 //MouseEventButton events
 mouseEventButton.addEventListener('mouseenter', () => {
-  mouseEventButton.textContent = 'Mouse hovered over!';
+  mouseEventButton.textContent = 'Click to submit!';
 });
 
 mouseEventButton.addEventListener('mouseleave', () => {
   mouseEventButton.textContent = 'Hover Me!';
 });
+
 const text1 = document.getElementById('text');
 // Textbox focus events
 text1.addEventListener('focus', () => {
@@ -18,7 +18,16 @@ text1.addEventListener('blur', () => {
     text1.placeholder = 'Click me!';
 });
 
+const lastKey = document.getElementById('lastKey');
 //Set last key typed in lastKey
 text1.addEventListener('keydown', (event) => {
   lastKey.textContent = event.key;
+});
+
+const form1 = document.getElementById('form1');
+// Form Submission
+form1.addEventListener('submit', (event) => {
+	console.log('Form submitted');
+	event.preventDefault();
+	alert(`${text.value}`);
 });
